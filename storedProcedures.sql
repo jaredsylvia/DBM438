c070d0940b8a71e537e83426ef340045010aebd4
Jared Sylvia <jaredsylvia@gmail.com>
2023-08-12 13:52:40 -0700

    Update to the actual working script
    Update to the actual working script

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -1,16 +1,17 @@
--- The stored procedures contains semicolons, so we need to change the delimiter here.
+-- The stored procedures contain semicolons, so we need to change the delimiter here.
 DELIMITER //
 
 CREATE PROCEDURE InsertAuthor(
+    IN p_AuthorID INT,
     IN p_Lname VARCHAR(50),
     IN p_Fname VARCHAR(50)
 )
 BEGIN
-    -- Check if the input data is valid, if not, raise an error. 
-    -- In this case the condition is that each value must be at least 1 character long.
-    IF LENGTH(p_Lname) > 0 AND LENGTH(p_Fname) > 0 THEN
-       -- If the input data is valid, insert the data into the table
-        INSERT INTO AUTHOR (Lname, Fname) VALUES (p_Lname, p_Fname);
+    -- Check if the input data is valid, if not, raise an error.
+    -- AuthorID should be an INT of minimum 4 characters other fiels greater than 0.
+    IF LENGTH(p_Lname) > 0 AND LENGTH(p_Fname) > 0 AND LENGTH(CAST(p_AuthorID AS CHAR)) >= 4 THEN
+       -- If the input data is valid, insert the data into the table.
+        INSERT INTO AUTHOR (AuthorID, Lname, Fname) VALUES (p_AuthorID, p_Lname, p_Fname);
                 
     ELSE
         -- If the input data is not valid, raise an error of type '45000' then set the message text.
@@ -21,34 +22,35 @@ END //
 -- Restore the delimiter to the default
 DELIMITER ;
 
--- Here we can see the stored procedure in the database
+-- To see the stored procedure in the database
 SHOW CREATE PROCEDURE InsertAuthor;
 
--- To use the stored prodedure we need to change the delimiter again..
+-- To use the stored procedure we need to change the delimiter again..
 DELIMITER //
 
 -- Call the stored procedure with valid input data
-CALL InsertAuthor('Doe', 'John')//
+CALL InsertAuthor(1234, 'Doe', 'John')//
 
 -- Now we'll try to call the stored procedure with invalid input data
-CALL InsertAuthor('', '')//
+CALL InsertAuthor(12, 'Invalid', 'Data')//
 
 -- Restore the delimiter to the default
 DELIMITER ;
 
--- Then show the data in the table
+-- Show the data in the table
 SELECT * FROM AUTHOR WHERE Lname = 'Doe' AND Fname = 'John';
 
--- TO see the procedure in the database
+-- To see the procedure in the database
 SHOW CREATE PROCEDURE InsertAuthor;
 
--- And finally drop the procedure
+-- Finally, drop the procedure
 DROP PROCEDURE InsertAuthor;
 
-/*
--- If you want to remove all the John Does after running this multiple times uncomment the following code
+
+
+-- Delete the John Doe record so that we can insert it again.
 DELETE FROM AUTHOR
-WHERE Lname = 'Doe' AND Fname = 'John';
-*/
+WHERE AuthorID = 1234;
+
 
 
